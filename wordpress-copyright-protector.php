21e8639b27d48f5fa134ef9144fbb21c9a4ced4f
gchokeen <gchokeen@gmail.com>
2014-04-28 16:58:18 +0530

    script added

--- wordpress-copyright-protector.php
+++ wordpress-copyright-protector.php
@@ -17,7 +17,6 @@ define('WPCP_PLUGIN_NAME', plugin_basename(__FILE__));
 define('WPCP_PLUGIN_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define('WPCP_PLUGIN_VERSION','0.1');
 
-require_once 'classes/WPCP_Core.php';
 require_once 'classes/settings.php';
 
 
@@ -158,8 +157,48 @@ if (!class_exists('Wp_Copyright_Protector')) {
 		}
 		
 		public function footerScript(){
-		  
 
+                $wpcp_general_settings = get_option('wpcp_general_settings');
+                
+                $wpcp_text_before_url = (!empty($wpcp_general_settings['wpcp_text_before_url'])?$wpcp_general_settings['wpcp_text_before_url']:'Read more at');
+                ?>
+                <script type="text/javascript">
+
+            
+                jQuery("body").bind('copy', function (e) {
+                        if (typeof window.getSelection == "undefined") return; //IE8 or earlier...
+                        
+                        var body_element = document.getElementsByTagName('body')[0];
+                        var selection = window.getSelection();
+                        
+                        //if the selection is short let's not annoy our users
+                        if (("" + selection).length < 30) return;
+                
+                        //create a div outside of the visible area
+                        //and fill it with the selected text
+                        var newdiv = document.createElement('div');
+                        newdiv.style.position = 'absolute';
+                        newdiv.style.left = '-99999px';
+                        body_element.appendChild(newdiv);
+                        newdiv.appendChild(selection.getRangeAt(0).cloneContents());
+                        
+                        //we need a <pre> tag workaround
+                        //otherwise the text inside "pre" loses all the line breaks!
+                        if (selection.getRangeAt(0).commonAncestorContainer.nodeName == "PRE") {
+                                newdiv.innerHTML = "<pre>" + newdiv.innerHTML
+                                + "</pre><br /><?php echo $wpcp_text_before_url; ?>: <a href='" + document.location.href + "'>"
+                                + document.location.href + "</a> &copy; MySite.com";
+                        }
+                        else
+                                newdiv.innerHTML += "<br /><br /><?php echo $wpcp_text_before_url; ?>: <a href='"
+                                + document.location.href + "'>"
+                                + document.location.href + "</a> &copy; MySite.com";
+                                        
+                        selection.selectAllChildren(newdiv);
+                        window.setTimeout(function () { body_element.removeChild(newdiv); }, 200);
+                });    
+                </script>
+                <?php
 		}	  
 
 		}
@@ -172,3 +211,4 @@ if (!class_exists('Wp_Copyright_Protector')) {
 if (class_exists('Wp_Copyright_Protector')) {
 	$Wp_Copyright_Protector =  Wp_Copyright_Protector::getInstance();
 }
+
